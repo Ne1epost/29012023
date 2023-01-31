0d6e62db20356091134b172759e56791784709ba
Ne1epost <119726311+Ne1epost@users.noreply.github.com>
2023-01-31 23:09:48 +0300

    Done

---
+++ Task64/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.void OutputNumbers(int m, int n) 
+
+int m = InputInt("Введите M: ");
+int n = InputInt("Введите N: ");
+Console.WriteLine($"Сумма элементов от {m} до {n} = {CountNaturalSum(m, n)}");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int CountNaturalSum(int m, int n)
+{
+    if (m == n)
+    return n;
+    return n + CountNaturalSum(m, n - 1);
+}
